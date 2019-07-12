20828444c00b383208f4778b02431083491c22a2
Till Müller <tillhendrikmueller@gmail.com>
2019-07-12 18:26:57 +0000

    Update debian-upgrade.sh

--- debian-upgrade.sh
+++ debian-upgrade.sh
@@ -6,11 +6,51 @@ fi
 
 needrestart
 apt-mark showhold
+
+read -p "No packages should be shown as on hold above. Continue with update & upgrade on current version?" -n 1 -r
+echo
+if [[ ! $REPLY =~ ^[Yy]$ ]]
+then
+    [[ "$0" = "$BASH_SOURCE" ]] && exit 1 || return 1
+fi
+
 apt update
 apt upgrade
+
+read -p "Continue with full upgrade?" -n 1 -r
+echo
+if [[ ! $REPLY =~ ^[Yy]$ ]]
+then
+    [[ "$0" = "$BASH_SOURCE" ]] && exit 1 || return 1
+fi
+
 apt full-upgrade
+
+read -p "Continue with autoremove?" -n 1 -r
+echo
+if [[ ! $REPLY =~ ^[Yy]$ ]]
+then
+    [[ "$0" = "$BASH_SOURCE" ]] && exit 1 || return 1
+fi
+
 apt autoremove
+
+read -p "Edit sources list to move stretch to buster?" -n 1 -r
+echo
+if [[ ! $REPLY =~ ^[Yy]$ ]]
+then
+    [[ "$0" = "$BASH_SOURCE" ]] && exit 1 || return 1
+fi
+
 sed -i 's/stretch/buster/g' /etc/apt/sources.list
 sed -i 's/stretch/buster/g' /etc/apt/sources.list.d/*.list
+
+read -p "Upgrade to buster?" -n 1 -r
+echo
+if [[ ! $REPLY =~ ^[Yy]$ ]]
+then
+    [[ "$0" = "$BASH_SOURCE" ]] && exit 1 || return 1
+fi
+
 apt update
 apt upgrade
